06b7052b4601219eafbd2577a4d0c0c866228a04
Emmanuel Mercado Luna <emmanuelf21004@gmail.com>
2025-02-23 18:13:33 -0400

    Delete unneeded comment

--- src/components/Questions/QuestionText.jsx
+++ src/components/Questions/QuestionText.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const QuestionText = ({ text }) => <h2 className="title is-4 mb-4">{text}</h2>;
 
 QuestionText.propTypes = {
-  text: PropTypes.string.isRequired,  // Ensure `text` is a required string
+  text: PropTypes.string.isRequired,
 };
 
 export default QuestionText;
